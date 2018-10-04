d01d90ebd44a7bc10a5bc4fb3ee6e5b0f7c8285f
mcbrided <dustin.mcbride@agilysys.com>
2018-10-03 21:13:08 -0700

    secret and port

--- src/api/middlewares/auth.js
+++ src/api/middlewares/auth.js
@@ -1,10 +1,12 @@
 const httpStatus = require('http-status');
 const APIError = require('../utils/APIError');
+const { secret } = require('../../config/vars');
+
 
 
 exports.authorize = () => (req, res, next) => {
   // TODO make legit
-  if (req.get('x-api-key') !== '123') {
+  if (req.get('x-api-key') !== secret ) {
     const apiError = new APIError({
       message: 'Unauthorized',
       status: httpStatus.UNAUTHORIZED,
--- src/config/vars.js
+++ src/config/vars.js
@@ -8,12 +8,13 @@
 
 module.exports = {
   env: process.env.NODE_ENV,
-  port: process.env.BIGMARIO_API_PORT,
+  port: process.env.PORT || 3000,
   mongo: {
     uri: process.env.BIGMARIO_API_MONGO_URI,
     password: process.env.BIGMARIO_API_MONGO_PASSWORD,
     user: process.env.BIGMARIO_API_MONGO_USER,
     dbName: process.env.BIGMARIO_API_MONGO_DB_NAME
   },
+  secret: process.env.BIGMARIO_API_MONGO_SECRET,
   logs: process.env.NODE_ENV === 'production' ? 'combined' : 'dev',
 };
